65c314c3dfc2bc56c285efecfac41dd576933cc7
Matthew Jagiello <mjagiello@gmail.com>
2020-02-04 05:25:24 +0000

    Automatically backed up by Learn

--- tools/console.rb
+++ tools/console.rb
@@ -13,8 +13,8 @@ mrkrabs = Instructor.new("mr krabs")
 spongebob_bt = spongebob.add_boating_test(self, "test 1", "pending", mspuff)
 patrick_bt = patrick.add_boating_test(self, "test 1", "pending", mrkrabs)
 
-#mspuff.pass_student("spongebob", "test 1")
-mspuff.pass_student("spongebob", "test 2")
+mspuff.pass_student("spongebob", "test 1")
+#mspuff.pass_student("spongebob", "test 2")
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
