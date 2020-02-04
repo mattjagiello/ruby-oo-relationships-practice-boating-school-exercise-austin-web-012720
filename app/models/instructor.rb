80a8c428909d51529741345d44646a0a4a323ffb
Matthew Jagiello <mjagiello@gmail.com>
2020-02-04 05:20:49 +0000

    Automatically backed up by Learn

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -17,7 +17,7 @@ class Instructor
         tests.boat_test_status = "passed"
       elsif tests.stud_name.name == student_name && tests.boat_test_name != test_name
         current_student = tests.stud_name.name == student_name
-        student_name.add_boating_test(student_name, test_name, "passed", self)
+        current_student.add_boating_test(student_name, test_name, "passed", self)
       end
     end
   end
