cb6cd704b3a493a5b0304790dd6722d2f2feffb4
Matthew Jagiello <mjagiello@gmail.com>
2020-02-04 04:47:58 +0000

    Automatically backed up by Learn

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,14 @@
 class Instructor
+  attr_accessor :inst_name
+  @@all = []
+
+  def initialize(inst_name)
+    @inst_name = inst_name
+    self.class.all << self
+  end
+
+  def self.all
+    @@all << self
+  end
 
 end
