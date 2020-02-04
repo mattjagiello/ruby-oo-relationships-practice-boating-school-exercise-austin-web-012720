992545e54e3551edf70f28fd8b0a95268ee46d23
Matthew Jagiello <mjagiello@gmail.com>
2020-02-04 04:50:50 +0000

    Automatically backed up by Learn

--- tools/console.rb
+++ tools/console.rb
@@ -8,7 +8,7 @@ spongebob = Student.new("spongebob")
 
 mspuff = Instructor.new("ms puff")
 
-spongebob_bt = spongebob.add_boating_test(self, )
+spongebob_bt = spongebob.add_boating_test(self, "test 1", "boat test taken")
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
