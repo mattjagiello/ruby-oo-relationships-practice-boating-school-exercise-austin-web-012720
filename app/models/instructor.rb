1d00804f69f77ef64c9538ba032d87bd474ce790
Matthew Jagiello <mjagiello@gmail.com>
2020-02-04 05:55:11 +0000

    Automatically backed up by Learn

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -14,8 +14,6 @@ class Instructor
   def pass_student(student_name, test_name)
     boating_test = BoatingTest.all.select{|x| x.stud_name == student_name && x.test_name == boat_test_name}
     boating_test.status = "passed"
-
-    if
   end
 
 end
