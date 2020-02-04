ce9ecbf5b68a0436e8f632a67c37b8577b2419c1
Matthew Jagiello <mjagiello@gmail.com>
2020-02-04 04:45:44 +0000

    Automatically backed up by Learn

--- app/models/boatingtest.rb
+++ app/models/boatingtest.rb
@@ -1,4 +1,11 @@
 class BoatingTest
   attr_accessor :stud_name, :boat_test_name, :boat_test_status
+  @@all = []
+
+  def initialize(stud_name, boat_test_name, boat_test_status)
+    @stud_name = stud_name
+    @boat_test_name = boat_test_name
+    @boat_test_status = boat_test_status
+    self.all.class << self
 
 end
