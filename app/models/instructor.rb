c7d20481c68a884837c127ebd74705124596a94f
Matthew Jagiello <mjagiello@gmail.com>
2020-02-04 05:14:14 +0000

    Automatically backed up by Learn

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -14,7 +14,7 @@ class Instructor
   def pass_student(student_name, test_name)
     BoatingTest.all.select do |tests|
       if tests.stud_name.name == student_name && tests.boat_test_name == test_name
-        tests.boat_test_name = "passed"
+        tests.boat_test_status = "passed"
       elsif tests.stud_name.name == student_name && tests.boat_test_name != test_name
         current_student = tests.stud_name.name == student_name
         current_student.add_boating_test(current_student, test_name, "passed", self)
