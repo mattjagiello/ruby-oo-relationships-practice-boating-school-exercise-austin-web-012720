0d8ea9280d92a8d8b7b966b3f2ef22e9e4158002
Matthew Jagiello <mjagiello@gmail.com>
2020-02-04 05:37:22 +0000

    Automatically backed up by Learn

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -19,6 +19,7 @@ class Instructor
       if tests.stud_name.name == student_name && tests.boat_test_name != test_name
           current_student = BoatingTest.all.collect do |tests|
             tests.stud_name.name == student_name
+          end
         BoatingTest.new(current_student, test_name, "passed", self)
       end
     end
