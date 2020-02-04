63b941ddb136515a127a2b3a610ee3f63fbc9a5b
Matthew Jagiello <mjagiello@gmail.com>
2020-02-04 05:06:09 +0000

    Automatically backed up by Learn

--- tools/console.rb
+++ tools/console.rb
@@ -5,10 +5,13 @@ def reload
 end
 
 spongebob = Student.new("spongebob")
+patrick = Student.new("patrick")
 
 mspuff = Instructor.new("ms puff")
+mrkrabs = Instructor.new("mr krabs")
 
 spongebob_bt = spongebob.add_boating_test(self, "test 1", "pending", mspuff)
+patrick_bt = patrick.add_boating_test(self, "test 1", "pending", mrkrabs)
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
